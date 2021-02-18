71806ea0fb205503cf1f19331679019e279c95d0
Melinda Ivanov <jns.melinda@gmail.com>
2021-02-17 20:14:09 -0800

    plugins

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -20,3 +20,28 @@ zinit light-mode for \
     zinit-zsh/z-a-bin-gem-node
 
 ### End of Zinit's installer chunk
+
+zinit wait lucid light-mode for \
+    atinit'ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay' \
+        zdharma/fast-syntax-highlighting \
+        OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh \
+    atload'_zsh_autosuggest_start' zsh-users/zsh-autosuggestions \
+    blockf atpull'zinit creinstall -q .' zsh-users/zsh-completions \
+    svn pick'completion.zsh' multisrc'git.zsh history.zsh' OMZ::lib
+
+zinit wait lucid light-mode for \
+    djui/alias-tips \
+    Tarrasch/zsh-bd
+
+zinit wait svn lucid light-mode for \
+    OMZ::plugins/z \
+    OMZ::plugins/gradle \
+    OMZ::plugins/mvn \
+    blockf OMZ::plugins/docker-compose
+
+zinit wait lucid light-mode for \
+    pick'.fzf.zsh' %HOME
+
+zinit lucid light-mode for \
+    svn OMZ::plugins/git \
+    pick'eaf.zsh' %HOME/.dotfiles/zsh
